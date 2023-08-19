79d01fdbe8f20ec8516c3e68ebffb32372644fa5
Vinetos <contact@vinetos.fr>
2023-08-19 16:23:17 +0200

    feat(desktop: kitty): Use my dracula based theme

--- home/terminals/kitty.nix
+++ home/terminals/kitty.nix
@@ -1,18 +1,20 @@
-{ ... }:
+{ default, ... }:
 let
-  darcula = builtins.fetchurl rec {
-    url = "https://raw.githubusercontent.com/dracula/kitty/master/dracula.conf";
-    sha256 = "139irq012z2yaxabmx9xn352knj1316qlyzn27lnfx5y9izkygri";
-  };
-
+  inherit (default) xcolors;
 in
 {
   programs.kitty = {
     enable = true;
     shellIntegration.enableFishIntegration = true;
-    extraConfig = "include ${darcula}";
     settings = {
       background_opacity = toString 0.7;
+
+      selection_foreground = xcolors.selection-foreground;
+      selection_background = xcolors.selection-background;
+
+      url_color = xcolors.cyan;
+
+      inherit (xcolors) foreground background color0 color1 color2 color3 color4 color5 color6 color7 color8 color9 color10 color11 color12 color13 color14 color15;
     };
   };
 }
