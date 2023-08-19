583422fbbd245f9c260cbdd9cbbf0a8d2c38dcea
Vinetos <contact@vinetos.fr>
2023-08-19 16:23:01 +0200

    feat(lib: theme): Setup dracula based theme

---
+++ lib/theme/colors.nix
@@ -0,0 +1,57 @@
+# Inspired from https://draculatheme.com/contribute#color-palette
+rec {
+
+  background = "282a36";
+  foreground = "f8f8f2";
+  selection-background = "44475a";
+  selection-foreground = white;
+  comment = "6272a4";
+
+  # Ansi mapping
+  color0 = bg-dark;
+  color1 = red;
+  color2 = green;
+  color3 = yellow;
+  color4 = blue;
+  color5 = pink;
+  color6 = cyan;
+  color7 = foreground;
+  color8 = comment;
+  color9 = light-red;
+  color10 = light-green;
+  color11 = light-yellow;
+  color12 = light-blue;
+  color13 = light-pink;
+  color14 = light-cyan;
+  color15 = white;
+
+  # Base colors
+  bg-lighter = "424450";
+  bg-light = "343746";
+  bg-darker = "191a21";
+  bg-dark = "21222c";
+
+  white = "ffffff";
+
+  red = "ff5555";
+  light-red = "ff6e6e";
+
+  green = "50fa7b";
+  light-green = "69ff94";
+
+  yellow = "f1fa8c";
+  light-yellow = "ffffa5";
+
+  blue = "bd93f9";
+  light-blue = "d6acff";
+
+  pink = "ff79c6";
+  light-pink = "ff92df";
+
+  cyan = "8be9fd";
+  light-cyan = "a4ffff";
+
+  orange = "ffb86c";
+  purple = "bd93f9";
+
+}
