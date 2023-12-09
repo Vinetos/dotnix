02ded4f6e2c18fba88504f4c8ec3ebaedbd1fac1
Vinetos <valentin@vinetos.fr>
2023-12-09 15:23:46 +0100

    chore(*): Reformat config

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -6,5 +6,5 @@
   programs.hyprland.enable = true;
   # add hyprland to display manager sessions
   services.xserver.displayManager.sessionPackages = [ inputs.hyprland.packages.${pkgs.hostPlatform.system}.default ];
-  security.pam.services.swaylock = {};
+  security.pam.services.swaylock = { };
 }
