03200d17efb03ccf7a0573e02fd16930f01c71bd
Valentin Chassignol <valentin.chassignol@infomaniak.com>
2024-02-20 12:40:05 +0100

    feat: Add XPS computer to the repo

--- hosts/default.nix
+++ hosts/default.nix
@@ -32,7 +32,7 @@
       modules =
         [
 
-          # inputs.nixos-hardware.nixosModules.framework-12th-gen-intel TODO: DO MR on nix-hardware for XPS 15 9570
+          inputs.nixos-hardware.nixosModules.dell-xps-15-9570-nvidia
           ./xps
           ../modules/doas.nix
           ../modules/xserver.nix
