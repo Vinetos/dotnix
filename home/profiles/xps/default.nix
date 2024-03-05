bb9e2cf99d63f6e5ca6021401325e2f83fe2adde
Valentin Chassignol <valentin.chassignol@infomaniak.com>
2024-03-05 13:52:10 +0100

    chore: home: profiles: xps: Update XPS custom config

--- home/profiles/xps/default.nix
+++ home/profiles/xps/default.nix
@@ -6,29 +6,30 @@
     ../../terminals/kitty.nix
   ];
 
-    # TODO: Replace with nix language when merged : https://github.com/nix-community/home-manager/pull/5038
-    wayland.windowManager.hyprland.extraConfig = ''
-      device {
-        name = mx-keys-keyboard
-        kb_layout = ch
-        kb_variant = fr
-      }
-      
-      device {
-        name = mx-keys-keyboard-2
-        kb_layout = ch
-        kb_variant = fr
-      }
+  # TODO: Replace with nix language when merged : https://github.com/nix-community/home-manager/pull/5038
+  wayland.windowManager.hyprland.extraConfig = ''
+    device {
+      name = mx-keys-keyboard
+      kb_layout = ch
+      kb_variant = fr
+    }
 
-      device {
-        name = at-translated-set-2-keyboard
-        kb_layout = ch
-        kb_variant = fr
-      }
-    '';
+    device {
+      name = mx-keys-keyboard-2
+      kb_layout = ch
+      kb_variant = fr
+    }
 
-    # Custom git configuration
-    programs.git = {
+    device {
+      name = at-translated-set-2-keyboard
+      kb_layout = ch
+      kb_variant = fr
+    }
+  '';
+
+  # Custom git configuration
+  programs = {
+    git = {
       # Signing Module
       signing = {
         key = "3EC1FE8DF090B8CF";
@@ -39,16 +40,29 @@
       userName = "Valentin Chassignol";
       userEmail = "valentin" + "." + "chassignol" + "@" + "infomaniak" + "." + "com";
     };
+    ssh = {
+      matchBlocks = {
+        all = {
+          host = "*";
+          user = "root";
+        };
+        infomaniak = {
+          host = "*.infomaniak.ch";
+          user = "root";
+        };
+      };
+    };
+  };
 
-    home.packages = with pkgs; [
-      openssl
-      traceroute
-      dig
-      wget
-      openvpn
-      remmina
-      virt-manager
-      kchat
-    ];
+  home.packages = with pkgs; [
+    openssl
+    traceroute
+    dig
+    wget
+    openvpn
+    remmina
+    virt-manager
+    kchat
+  ];
 
 }
