e9619c2305ab44fe2144fe43c942c7a5ddda63c7
Valentin Chassignol <valentin.chassignol@infomaniak.com>
2024-03-20 09:48:56 +0100

    fix: wayland: revert idea fix to old config

--- home/wayland/hyprland/config.nix
+++ home/wayland/hyprland/config.nix
@@ -169,20 +169,14 @@ let
   '';
   # https://github.com/hyprwm/Hyprland/issues/1947
   idea-fix = ''
-    # -- Fix odd behaviors in IntelliJ IDEs --
-    #! Fix focus issues when dialogs are opened or closed
-    windowrulev2 = windowdance,class:^(jetbrains-.*)$,floating:1
-    #! Fix splash screen showing in weird places and prevent annoying focus takeovers
-    windowrulev2 = center,class:^(jetbrains-.*)$,title:^(splash)$,floating:1
-    windowrulev2 = nofocus,class:^(jetbrains-.*)$,title:^(splash)$,floating:1
-    windowrulev2 = noborder,class:^(jetbrains-.*)$,title:^(splash)$,floating:1
-
-    #! Center popups/find windows
-    windowrulev2 = center,class:^(jetbrains-.*)$,title:^( )$,floating:1
-    windowrulev2 = stayfocused,class:^(jetbrains-.*)$,title:^( )$,floating:1
-    windowrulev2 = noborder,class:^(jetbrains-.*)$,title:^( )$,floating:1
-    #! Disable window flicker when autocomplete or tooltips appear
-    windowrulev2 = nofocus,class:^(jetbrains-.*)$,title:^(win.*)$,floating:1
+    windowrulev2=windowdance,class:^(jetbrains-.*)$
+    # search dialog
+    windowrulev2=dimaround,class:^(jetbrains-.*)$,floating:1,title:^(?!win)
+    windowrulev2=center,class:^(jetbrains-.*)$,floating:1,title:^(?!win)
+    # autocomplete & menus
+    windowrulev2=noanim,class:^(jetbrains-.*)$,title:^(win.*)$
+    windowrulev2=noinitialfocus,class:^(jetbrains-.*)$,title:^(win.*)$
+    windowrulev2=rounding 0,class:^(jetbrains-.*)$,title:^(win.*)$
   '';
 in
 {
