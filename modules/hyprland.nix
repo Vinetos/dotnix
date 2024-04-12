d31f24df987316dd7bec46ba290078d7edec6827
Valentin Chassignol <valentin.chassignol@infomaniak.com>
2024-04-12 12:47:25 +0200

    chore: modules: hyprland: Switch services.xserver.displayManager.XXX settings to services.displayManager.XXX

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -5,6 +5,6 @@
 }: {
   programs.hyprland.enable = true;
   # add hyprland to display manager sessions
-  services.xserver.displayManager.sessionPackages = [ inputs.hyprland.packages.${pkgs.hostPlatform.system}.default ];
+  services.displayManager.sessionPackages = [ inputs.hyprland.packages.${pkgs.hostPlatform.system}.default ];
   security.pam.services.swaylock = { };
 }
