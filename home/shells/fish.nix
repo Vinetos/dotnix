454aa246a613751217c867e9caa9a067a09bce66
Valentin Chassignol <valentin.chassignol@infomaniak.com>
2024-04-12 12:39:54 +0200

    feat: home: shells: Add nrb shell abbrevation

--- home/shells/fish.nix
+++ home/shells/fish.nix
@@ -13,6 +13,7 @@
       nr = "nix run nixpkgs#";
       nrs = "sudo nixos-rebuild switch --flake /etc/nixos/#(hostname)";
       nri = "sudo nixos-rebuild switch --flake /etc/nixos/#(hostname) --impure";
+      nrb = "sudo nixos-rebuild boot --flake /etc/nixos/#(hostname)";
       nu = "nix flake update";
       ngc = "sudo nix-collect-garbage -d";
 
