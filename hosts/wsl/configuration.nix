8cffd8c04dc2c6e3b82cc022441b75066b6cb88e
Valentin Chassignol <valentin.chassignol@infomaniak.com>
2024-06-24 10:25:07 +0200

    chore: host: wsl: lint

--- hosts/wsl/configuration.nix
+++ hosts/wsl/configuration.nix
@@ -37,7 +37,7 @@
     autoPrune.enable = true;
   };
 
-   ## patch the script
+  ## patch the script
   systemd.services.docker-desktop-proxy.script = lib.mkForce ''${config.wsl.wslConf.automount.root}/wsl/docker-desktop/docker-desktop-user-distro proxy --docker-desktop-root ${config.wsl.wslConf.automount.root}/wsl/docker-desktop "C:\Program Files\Docker\Docker\resources"'';
 
   #users.users.vinetos.uid = 1000;
