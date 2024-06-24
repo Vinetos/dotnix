bee76504e64b2e251e8908d47da108e50d332443
Valentin Chassignol <valentin.chassignol@infomaniak.com>
2024-06-24 10:24:45 +0200

    feat: hosts: xps: Enable libvirtd

--- hosts/xps/hardware-configuration.nix
+++ hosts/xps/hardware-configuration.nix
@@ -49,5 +49,21 @@
   services.blueman.enable = true;
 
   virtualisation.docker.enable = true;
-  virtualisation.libvirtd.enable = true;
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = true;
+      swtpm.enable = true;
+      ovmf = {
+        enable = true;
+        packages = [
+          (pkgs.OVMF.override {
+            secureBoot = true;
+            tpmSupport = true;
+          }).fd
+        ];
+      };
+    };
+  };
 }
