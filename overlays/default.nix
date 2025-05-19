fe76a50072df8968037bc6b20769b64877bdfebb
Valentin Chassignol <valentin.chassignol@infomaniak.com>
2025-05-19 15:51:51 +0200

    feat: packages: Package openstack-exporter go module

--- overlays/default.nix
+++ overlays/default.nix
@@ -7,4 +7,5 @@ let
 in
 self: super: {
   desktime = self.callPackage "${packages}/desktime.nix" { };
+  openstack-exporter = self.callPackage "${packages}/openstack-exporter.nix" { };
 }
--- packages/default.nix
+++ packages/default.nix
@@ -9,4 +9,5 @@ self: super: {
   bibata-hyprcursor = self.callPackage "${packages}/bibata-hyprcursor" { };
   zen-browser = inputs.zen-browser.packages."${self.system}".specific;
   desktime = self.callPackage "${packages}/desktime.nix" { };
+  openstack-exporter = self.callPackage "${packages}/openstack-exporter.nix" { };
 }
