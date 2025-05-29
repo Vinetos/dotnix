0d4f61315b878e1170bd9f7cd738a4261bc9cfc7
Vinetos <valentin@vinetos.fr>
2025-05-29 17:01:26 +0200

    feat: home: mako: use new settings options

--- modules/home/wayland/mako.nix
+++ modules/home/wayland/mako.nix
@@ -10,17 +10,18 @@ in
 {
   services.mako = {
     enable = true;
-
-    sort = "-time";
-    layer = "top";
-    backgroundColor = xcolors.background + "EF"; # Add opacity
-    textColor = xcolors.selection-foreground;
-    margin = toString 0;
-    padding = toString 16;
-    borderSize = 0;
-    borderRadius = 12;
-    icons = true;
-    defaultTimeout = 15 * 1000; # 30s
+    settings = {
+      sort = "-time";
+      layer = "top";
+      backgroundColor = xcolors.background + "EF"; # Add opacity
+      textColor = xcolors.selection-foreground;
+      margin = toString 0;
+      padding = toString 16;
+      borderSize = 0;
+      borderRadius = 12;
+      icons = true;
+      defaultTimeout = 15 * 1000; # 30s
+    };
   };
   #icon-path=/usr/share/icons/Papirus-Dark
   #font=JetBrainsMono Nerd Font 10
