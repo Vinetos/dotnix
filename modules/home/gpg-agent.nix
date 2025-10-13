68c2c9c321f3377766fb6f5dec11d68113a251d3
Valentin Chassignol <valentin.chassignol@infomaniak.com>
2025-10-13 11:31:00 +0200

    feat(home: gpg-agent): Disable disable-ccid

--- modules/home/gpg-agent.nix
+++ modules/home/gpg-agent.nix
@@ -5,4 +5,7 @@
     enableSshSupport = true;
     pinentry.package = pkgs.pinentry-gnome3;
   };
+
+  # This avoids the problem where GnuPG will repeatedly prompt for the insertion of an already-inserted YubiKey
+  programs.gpg.scdaemonSettings.disable-ccid = true;
 }
