f21b024d8520f7b997ee173e2b9248de7f2605b0
Valentin Chassignol <valentin.chassignol@infomaniak.com>
2026-08-15 11:41:55 +0200

    feat(home): Remove shell config

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -9,16 +9,6 @@
       '';
     };
 
-    # For macOS's default shell.
-    zsh = {
-      enable = true;
-      autosuggestion.enable = true;
-      syntaxHighlighting.enable = true;
-      envExtra = ''
-        # Custom zshrc goes here
-      '';
-    };
-
     nushell.enable = true;
 
     # Type `z <pat>` to cd to some directory
